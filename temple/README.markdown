a29d391019f2c0a9d486c60153b6f9ee3ffe4248
surveyormarket <77161648+surveyormarket@users.noreply.github.com>
2021-01-17 12:40:46 -0500

    Add files via upload

---
+++ temple/README.markdown
@@ -0,0 +1,5 @@
+# Temple Hamburger Menu Concept
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/JFarrow/pen/rNMqzVW](https://codepen.io/JFarrow/pen/rNMqzVW).
+
+Menu concept using the existing temple logo design of http://mbtkc.org/. Love the idea but fear the usability would be awful... I'd love to see someone fork this and figure out a subtle way to clue the user into the menu button functionality
---
+++ temple/src/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+    $('.mobile-nav').click(function() {
+	   $('.mobile-nav__icon').toggleClass('active'); 
+        $('.menu').slideToggle();
+    });
+});
+
+// Aria Compliance
+
+  function handleBtnClick(event) {
+    event = event || window.event;
+    var pressed = event.target.getAttribute("aria-pressed") == "true";
+    //change the aria-pressed value as the button is toggled:
+    event.target.setAttribute("aria-pressed", pressed ? "false" : "true");
+    //... (perform the button's logic here)
+  }
+
+  function handleBtnKeyUp(event) {
+    event = event || window.event;
+    if (event.keyCode === 32) { // check for Space key
+      handleBtnClick(event);
+    }
+  }
+ 
